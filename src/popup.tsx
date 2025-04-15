fddb4ae51d88fa0b31ed939533142137eb4c01ea
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-04-15 16:31:18 -0400

    added tailwindcss

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react"
+import "./style.css"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+    <div className="p-4 text-center bg-blue-500 text-white rounded-lg shadow-md">
+      Hello from Tailwind!
     </div>
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     padding: 16,
+    //     width:400,
+    //   }}>
+    //   <h1>
+    //     Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+    //   </h1>
+    //   <input onChange={(e) => setData(e.target.value)} value={data} />
+    //   <footer>Crafted by @PlasmoHQ</footer>
+    // </div>
   )
 }
 
