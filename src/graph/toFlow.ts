a5b64fb79728e5e3dc29e2dc40bdf6db91867c84
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-04-18 21:12:54 -0400

    cleaned up code

--- src/graph/toFlow.ts
+++ src/graph/toFlow.ts
@@ -9,30 +9,23 @@ export function convertGraphToFlow(graph: GraphNode[]): {
   const nodes: Node[] = []
   const edges: Edge[] = []
 
-  let x = 0
-  let y = 0
-
   for (const node of graph) {
     // Handle node
     nodes.push({
       id: node.id,
       data: { label: "", timestamp: node.timeStamp },
-      position: { x: x, y: y }, // TODO: use layout engine to modify (use Omit to ignore this field)
+      position: { x: 0, y: 0 }, // TODO: use layout engine to modify (use Omit to ignore this field)
       width: DEFAULT_NODE_DIMENSIONS,
       height: DEFAULT_NODE_DIMENSIONS,
-
+      connectable: false,
     })
-
-    // TODO: remove positions...
-    // x += 100
-    // y += 100
 
     // Handle edge
     for (const childId of node.children) {
       edges.push({
         id: `e[${node.id}]-[${childId}]`,
         source: node.id,
-        target: childId,
+        target: childId
       })
     }
   }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,23 +1,30 @@
 import "@xyflow/react/dist/style.css"
 import "@/styles/globals.css"
 
-import { Background, BackgroundVariant, ReactFlow, useEdgesState, useNodesState, type Edge, type Node } from "@xyflow/react"
+import { convertGraphToFlow } from "@/graph/toFlow"
+import type { GraphNode } from "@/types/graph"
+import {
+  Background,
+  BackgroundVariant,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  type Edge,
+  type Node
+} from "@xyflow/react"
 import { useEffect } from "react"
-import { convertGraphToFlow } from "@/graph/toFlow";
-import type { GraphNode } from "@/types/graph";
-import applyDagreLayout from "./graph/toLayout";
+
+import applyDagreLayout from "./graph/toLayout"
 
 function IndexPopup() {
-  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
-
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([])
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([])
 
   useEffect(() => {
     chrome.runtime.sendMessage({ type: "GET_GRAPH" }, (response) => {
       if (response?.graph) {
-        const graph: GraphNode[] = response.graph;
+        const graph: GraphNode[] = response.graph
         const rawFlow = convertGraphToFlow(graph)
-        console.log("[IndexPopup] converted graph", rawFlow);
         const layoutFlow = applyDagreLayout(rawFlow.nodes, rawFlow.edges)
         setNodes(layoutFlow.nodes)
         setEdges(layoutFlow.edges)
@@ -27,7 +34,7 @@ function IndexPopup() {
 
   return (
     <div className="w-96 h-96">
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow nodes={nodes} edges={edges} fitView>
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
       </ReactFlow>
     </div>
